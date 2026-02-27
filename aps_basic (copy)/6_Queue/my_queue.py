25f6e3075018f68c1bbcd3be47016f0ac2969533
홍채원 <sophia1919@naver.com>
2026-02-27 17:48:29 +0900

    0227

--- aps_basic (copy)/6_Queue/my_queue.py
+++ aps_basic (copy)/6_Queue/my_queue.py
@@ -3,7 +3,7 @@
 def bfs(G,v,n): # 그래프 G, 탐색 시작점 v...
     visited = [0]*(n+1)
     queue = []
-    queue.append(v)
+    queue.append(v) # append 써서 표기하면 그래도 괜찮음
     visited[v]=1
     while queue:
         t = queue.pop(0)
